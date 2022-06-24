ec10752956855a9956a2022a95f1fa4939825c23
Eldes <studio@eldes.com>
2022-06-24 17:36:27 -0300

    [+] Exibe mensagem para selecionar conteúdo

--- src/pages/Event/index.tsx
+++ src/pages/Event/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowCircleRight } from 'phosphor-react';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
@@ -13,7 +14,17 @@ const EventPage = () => {
     <div className='flex flex-col min-h-screen'>
       <Header />
       <main className='flex flex-1'>
-        {slug ? <Video lessonSlug={slug} /> : <div className='flex-1'/>}
+        {(slug) ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className='flex-1 pt-[40vh] text-gray-300 text-xl'>
+            <span className='flex flex-row items-center justify-center gap-2'>
+              Escolha algum dos conteúdos liberados
+              <ArrowCircleRight size={24} weight="light" className='animate-pulse text-blue-500' />
+            </span>
+            
+          </div>
+        )}
         <Sidebar />
       </main>      
     </div>
